f1340f734cd1de678ff483c176c6ecccabd52fed
TI2042 <55748500+TI2042@users.noreply.github.com>
2020-03-16 15:57:09 +0400

    Lab1

---
+++ SecuritySystemView/Program.cs
@@ -0,0 +1,36 @@
+﻿using SecuritySystemListImplement.Implements;
+using SecuritySystemsBusinessLogic.Interfaces;
+using SecuritySystemsBusinessLogic.BusinessLogic;
+using System;
+using System.Windows.Forms;
+using Unity;
+using Unity.Lifetime;
+using SecuritySysytemImplement.Implements;
+
+namespace SecuritySystemView
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var container = BuildUnityContainer();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(container.Resolve<FormMain>());
+        }
+        private static IUnityContainer BuildUnityContainer()
+        {
+            var currentContainer = new UnityContainer();
+            currentContainer.RegisterType<IDeviceLogic, DeviceLogic>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IOrderLogic, OrderLogic>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IEquipmentLogic, EquipmentLogic>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<MainLogic>(new HierarchicalLifetimeManager());
+
+            return currentContainer;
+        }
+    }
+}
